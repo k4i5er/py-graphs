f01c47957bf97ce29af7e8c349cbe7740147a665
AzuUl46 <monselunaazul325@gmail.com>
2022-10-15 15:46:53 -0500

    design

--- 03_graphics.py
+++ 03_graphics.py
@@ -1,5 +1,4 @@
-from __future__ import division
-from tkinter import Tk, Canvas, StringVar
+from tkinter import LEFT, Tk, Canvas, StringVar
 from tkinter.ttk import Frame, Combobox, Label, Entry
 from math import sqrt
 
@@ -51,20 +50,32 @@ class Model():
         params = self.plane.get_params()
         canvas = self.plane.get_canvas()
         divisions = self.plane.get_divisions()
-        x_coord = params[0] * divisions[0]
-        y_coord = params[1] * divisions[1]
+        origin = self.plane.get_origin()
+        print(origin)
+        print(361//divisions[0])
         print('>>>>>', canvas)
         # Para graficar el modelo lineal
         if self.model == 'Lineal':
+            x_coord = params[0] * divisions[1]
+            y_coord = params[1] * divisions[1]
             for i in range(-self.dom_x, self.dom_x):
                 origin = self.plane.get_origin()
                 canvas.create_line(
                     i*divisions[0] + origin[0], origin[1]-(x_coord*i+y_coord), (i+1)*divisions[0]+origin[0], origin[1]-(x_coord*(i+1)+y_coord))
         elif self.model == 'Cuadrático':
-            for i in range(-self.dom_x, self.dom_x):
-                origin = self.plane.get_origin()
-                canvas.create_line(
-                    i*divisions[0] + origin[0], origin[1]-(x_coord*i+y_coord), (i+1)*divisions[0]+origin[0], origin[1]-(x_coord*(i+1)+y_coord))
+            a = params[0]  * divisions[1]
+            b = params[1]  * divisions[1]
+            c = params[2]  * divisions[1]
+            i = -self.dom_x
+            while i <= self.dom_x:
+                x_coord = i*divisions[0]
+                x_coord2 = (i+1)*divisions[0]
+                k = x_coord2 - x_coord
+                for j in range(k):
+                    canvas.create_line(
+                        j + x_coord + origin[0], origin[1]-(a*(i+j/k)**2+b*(i+j/k)+c), (j+1) + x_coord + origin[0], origin[1]-(a*(i + (j+1)/k)**2+b*(i + (j+1)/k)+c))
+                i += 1
+                
 
             # canvas.create_line(0, 0, 300, 600)
             # print('Graficando... (próximamente)')
@@ -87,28 +98,14 @@ class Draw(Tk):
         # Widgets
         self.frm_eq = Frame(self, relief='sunken')
         self.frm_eq_list = Frame(self.frm_eq)
-        self.cmb_eq_list = Combobox(self.frm_eq_list)
+        self.cmb_eq_list = Combobox(self.frm_eq_list, state='readonly')
         self.cmb_eq_list.pack()
         self.frm_eq_list.pack(ipady=10)
-        self.frm_models = Frame(self.frm_eq)
-        Label(self.frm_models, text='y =').pack(side='left')
-        self.param1 = StringVar()
-        self.entry_param1 = Entry(
-            self.frm_models, textvariable=self.param1, width=4)
-        self.entry_param1.pack(side='left')
-        self.entry_param1.bind(
-            '<Return>', lambda e: self.entry_param2.focus_set())
-        Label(self.frm_models, text=' x + ').pack(side='left')
-        self.param2 = StringVar()
-        self.entry_param2 = Entry(
-            self.frm_models, textvariable=self.param2, width=4)
-        self.entry_param2.pack(side='left')
-        self.entry_param2.bind('<Return>', lambda e: self.model_graph(
-            self.cmb_eq_list.get()))
-        self.frm_models.pack(ipady=10)
         self.frm_eq.pack(side='left', fill='both', expand=1, pady=10)
-
+        
+        self.cmb_eq_list.bind('<<ComboboxSelected>>', lambda event: self.get_graf(self.cmb_eq_list))
         # Props
+        
         self.width = self.canvas.winfo_width()  # self.winfo_width()
         self.height = self.canvas.winfo_height()  # self.winfo_height()
         self.dom_x = 10
@@ -183,6 +180,45 @@ class Draw(Tk):
         #############################################################
         if self.cmb_eq_list.selection_get() == 'Lineal':
             return (float(self.param1.get()), float(self.param2.get()))
+        elif self.cmb_eq_list.selection_get() == 'Cuadrático':
+            self.param1 = StringVar()
+            self.param2 = StringVar()
+            self.param3 = StringVar()
+            self.param1.set('-1')
+            self.param2.set('0')
+            self.param3.set('0')
+            return (float(self.param1.get()), float(self.param2.get()), float(self.param3.get()))
+   
+    def get_graf(self, opc):
+        self.frm_models = Frame(self.frm_eq)
+        self.frm_models.pack(ipady= 10)
+
+        self.label = Label(self.frm_models)
+        self.label.pack(side=LEFT)
+
+        self.param1= StringVar()
+        self.entry_param1 = Entry(self.frm_models, textvariable=self.param1, width=4)
+        self.entry_param1.pack(side=LEFT)
+        self.entry_param1.bind('<Return>', lambda e: self.entry_param2.focus_set())
+
+        self.label_1 = Label(self.frm_models)
+        self.label_1.pack(side=LEFT)
+
+        self.param2= StringVar()
+        self.entry_param2 = Entry(self.frm_models, textvariable=self.param2, width=4)
+        self.entry_param2.pack(side=LEFT)  
+        self.entry_param2.bind('<Return>', lambda e: self.model_graph(self.cmb_eq_list.get() ) )
+
+        self.label_2 = Label(self.frm_models)
+        self.label_2.pack(side=LEFT)
+
+        if opc.get() == 'Lineal':
+            self.label.config(text='y =') 
+            self.label_1.config(text='x + ') 
+
+        if opc.get() == 'Cuadrático':
+            pass
+       
 
     def model_graph(self, model_name):
         # params = self.get_params()
